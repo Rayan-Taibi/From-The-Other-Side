8630470163d27dc416cb3c530d937213514be3d6
Rayane TAIBI <taibirayane86@gmail.com>
2026-04-13 16:10:10 +0100

    Update index.html and serveStatic.js to serve the index page correctly

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,12 +1,16 @@
 import path from 'node:http'
+import fs from 'node:fs/promises'
+import {sendResponse} from './sendRespond.js'
 
 export async function serveStatic(req , res , baseDir){
 
   
 
-    const filePath = path.join(baseDir , req.url === '/' ? 'index.html' : req.url) // here we are joining the base directory with the requested url. If the url is '/' then we will serve 'index.html' file. if not then we will serve the requested file.
+    const filePath = path.join(baseDir ,'public' , 'index.html' ) // here we are joining the base directory with the requested url. If the url is '/' then we will serve 'index.html' file. if not then we will serve the requested file.
     
     try{
+        const content = await fs.readFile(filePath)
+        sendResponse(res , 200 , 'text/html ' , content)
 
     }catch(err){
         console.log(err)
