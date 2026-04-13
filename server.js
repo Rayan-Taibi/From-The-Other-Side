0751f905ce40e7c5eface3e648afe1796fbd919f
Rayane TAIBI <taibirayane86@gmail.com>
2026-04-13 16:16:47 +0100

    Enhance index.html structure and styling; update server.js and serveStatic.js for improved functionality

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import http from 'node:http'
-import {serveStatic} from './utils/serveStatic'
-
+import {serveStatic} from './utils/serveStatic.js'
 
 const PORT = 8000
 const __dirname = import.meta.dirname
-
+console.log(__dirname)
 const server = http.createServer(async (req,res) => {
     await serveStatic(req, res, __dirname)
 })
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,4 +1,4 @@
-import path from 'node:http'
+import path from 'node:path'
 import fs from 'node:fs/promises'
 import {sendResponse} from './sendRespond.js'
 
@@ -6,11 +6,11 @@ export async function serveStatic(req , res , baseDir){
 
   
 
-    const filePath = path.join(baseDir ,'public' , 'index.html' ) // here we are joining the base directory with the requested url. If the url is '/' then we will serve 'index.html' file. if not then we will serve the requested file.
+    const filePath = path.join(baseDir, 'index.html' ) // here we are joining the base directory with the requested url. If the url is '/' then we will serve 'index.html' file. if not then we will serve the requested file.
     
     try{
         const content = await fs.readFile(filePath)
-        sendResponse(res , 200 , 'text/html ' , content)
+        sendResponse(res , 200 , 'text/html' , content)
 
     }catch(err){
         console.log(err)
